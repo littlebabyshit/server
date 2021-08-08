2f379585e10658ed2999735bf67b509bf8444f39
littlebabyshit <935396509@qq.com>
2021-08-08 21:20:09 +0800

    add code

---
+++ main.py
@@ -0,0 +1,14 @@
+import flask
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["get", "post"])
+def get_message():
+    print(request.json)
+
+    return "yes"
+
+
+if __name__ == '__main__':
+    app.run()
